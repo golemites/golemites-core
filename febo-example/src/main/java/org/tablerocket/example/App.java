03bf2a0d07e6803c71636a80916a3b23fc7b9694
Toni Menzel <toni.menzel@rebaze.com>
2018-02-27 12:46:30 +0100

    correct packagename for generated example repo

--- febo-example/src/main/java/org/tablerocket/example/App.java
+++ febo-example/src/main/java/org/tablerocket/example/App.java
@@ -1,20 +1,18 @@
 package org.tablerocket.example;
 
-import com.foo.bar.FeboRepository;
-
 import static org.ops4j.pax.tinybundles.core.TinyBundles.bundle;
 import static org.tablerocket.febo.core.Febo.febo;
 
 public class App
 {
-    public static void main(String[] args) throws Exception
+    public static void main( String[] args ) throws Exception
     {
         FeboRepository repo = new FeboRepository();
 
         febo()
-            .require( repo.org_apache_felix_configadmin())
+            .require( repo.org_apache_felix_configadmin() )
             .require( repo.org_apache_felix_scr() )
             .with( "booking", bundle().add( BookingService.class ) )
-            .run(args);
+            .run( args );
     }
 }
