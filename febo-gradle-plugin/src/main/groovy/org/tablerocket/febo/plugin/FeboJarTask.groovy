008a258e1b8e766293d92161203bbffd44d9bb4c
Toni Menzel <toni.menzel@rebaze.com>
2019-03-22 13:38:15 +0100

    move to aqute bnd plugin

--- febo-gradle-plugin/src/main/groovy/org/tablerocket/febo/plugin/FeboJarTask.groovy
+++ febo-gradle-plugin/src/main/groovy/org/tablerocket/febo/plugin/FeboJarTask.groovy
@@ -1,8 +1,11 @@
 package org.tablerocket.febo.plugin
 
+import org.gradle.api.file.RelativePath
 import org.gradle.api.internal.file.copy.CopyAction
+import org.gradle.api.internal.file.copy.CopyActionProcessingStream
+import org.gradle.api.internal.file.copy.FileCopyDetailsInternal
 import org.gradle.api.tasks.CacheableTask
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.WorkResult
 import org.gradle.api.tasks.bundling.Jar
 
 @CacheableTask
@@ -18,5 +21,27 @@ class FeboJarTask extends Jar {
             return copyAction;
         }
         return new ReproducibleOrderingCopyAction(copyAction);
+    }
+
+    private static final class ReproducibleOrderingCopyAction implements CopyAction {
+
+        private final CopyAction delegate;
+
+        private ReproducibleOrderingCopyAction(CopyAction delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        WorkResult execute(CopyActionProcessingStream stream) {
+            return this.delegate.execute({ action ->
+                Map<RelativePath, FileCopyDetailsInternal> detailsByPath = new TreeMap<>();
+                stream.process({ details ->
+                    detailsByPath.put(details.getRelativePath(),
+                            details)
+                });
+                detailsByPath.values().forEach(action.&processFile);
+            });
+        }
+
     }
 }
