3b4bfbdcc32d754f5debd56dd5ce2e88588f2b74
Toni Menzel <toni.menzel@rebaze.com>
2019-07-23 09:28:14 +0200

    remove not used method

--- febo-api/src/main/java/org/tablerocket/febo/api/Febo.java
+++ febo-api/src/main/java/org/tablerocket/febo/api/Febo.java
@@ -1,7 +1,5 @@
 package org.tablerocket.febo.api;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -19,9 +17,6 @@ public interface Febo extends AutoCloseable {
 
     @Deprecated
     Febo require(Dependency... identifiers);
-
-    @Deprecated
-    Febo require(String label, InputStream payload) throws IOException;
 
     <T> Optional<T> service(Class<T> clazz) throws Exception;
 
