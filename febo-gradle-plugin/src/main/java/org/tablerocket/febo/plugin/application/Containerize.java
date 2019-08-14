99aa4690220e4836018e54470adf2b492836add9
Toni Menzel <toni.menzel@rebaze.com>
2019-08-14 11:22:14 +0200

    constants!

--- febo-gradle-plugin/src/main/java/org/tablerocket/febo/plugin/application/Containerize.java
+++ febo-gradle-plugin/src/main/java/org/tablerocket/febo/plugin/application/Containerize.java
@@ -9,6 +9,11 @@ import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 
 public class Containerize {
+
+    public static final String BASE_IMAGE = "openjdk:8-jre-alpine";
+    public static final String LATEST_VERSION = "latest";
+    public static final String JAVA_PATH = "/usr/bin/java";
+
     private final String name;
 
     public Containerize(String name) {
@@ -18,12 +23,12 @@ public class Containerize {
     public String containerize(File output) throws IOException {
         // TODO: add dependencies into separate layers.
         try {
-            JibContainer result = Jib.from("openjdk:8-jre-alpine")
+            JibContainer result = Jib.from(BASE_IMAGE)
                     .addLayer(Collections.singletonList(output.toPath()), AbsoluteUnixPath.get("/"))
-                    .setEntrypoint("/usr/bin/java", "-jar", "/" + output.getName())
+                    .setEntrypoint(JAVA_PATH, "-jar", "/" + output.getName())
                     .setCreationTime(Instant.now())
                     .containerize(
-                            Containerizer.to(DockerDaemonImage.named(name + ":latest")
+                            Containerizer.to(DockerDaemonImage.named(name + ":" + LATEST_VERSION)
                             ));
             return result.getImageId().getHash();
         } catch (InterruptedException | RegistryException | CacheDirectoryCreationException | ExecutionException | InvalidImageReferenceException e) {
