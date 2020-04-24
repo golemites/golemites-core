d06ff8c890f7432084bd54f68f414a9509ee90c7
Toni Menzel <toni.menzel@rebaze.com>
2020-04-25 00:51:43 +0200

    re-introduce auto bundler in a lab testcase

--- golemites-sdk/src/test/java/org/rebaze/osgi/OSGiRuntimeTest.java
+++ golemites-sdk/src/test/java/org/rebaze/osgi/OSGiRuntimeTest.java
@@ -1,8 +1,10 @@
 package org.rebaze.osgi;
 
+import org.golemites.autobundle.AutoBundleSupport;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.osgi.framework.BundleException;
+import org.rebaze.osgi.testbundles.OneBundle;
 
 public class OSGiRuntimeTest {
 
@@ -10,7 +12,9 @@ public class OSGiRuntimeTest {
     @Disabled
     void testRuntime() throws Exception {
         OSGiRuntime osgi = null;
+        AutoBundleSupport auto = new AutoBundleSupport();
         Febo.febo()
+                .require(auto.from(OneBundle.class).withBndHeader("Export-Package",OneBundle.class.getPackage().getName()))
                 .run(null)
                 ;
     }
